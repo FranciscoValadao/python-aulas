36d9743b0381bb4e6fd94c442660b35a2089472f
Francisco Valadão <farialotti@gmail.com>
2021-12-08 22:37:00 -0300

    instaurado loop e contagem de rodadas

--- adivinhacao.py
+++ adivinhacao.py
@@ -2,18 +2,25 @@ print ("*******************")
 print ("Jogo de adivinhação")
 print ("*******************")
 
+total_tentativas= 3
 numero_secreto = 42
-chute_inp = input("digite o seu número:")
-chute = int(chute_inp)
-print('voce digitou:',chute)
-maior = (chute > numero_secreto)
-menor = (chute < numero_secreto)
+rodada = 1
 
-if numero_secreto == chute:
-    print ("acertou")
-else:
-    if (maior):
-        print ("achou muito, otário")
-    elif(menor):
-        print("achou pouco, otário")
+while(total_tentativas>0):
+    print("tentativa", rodada, "de 3")
+    rodada = rodada+1
+    chute_inp = input("digite o seu número:")
+    chute = int(chute_inp)
+    print('voce digitou:',chute)
+    maior = (chute > numero_secreto)
+    menor = (chute < numero_secreto)
 
+    if numero_secreto == chute:
+        print ("acertou")
+        break
+    else:
+        if (maior):
+            print ("achou muito, otário")
+        elif(menor):
+            print("achou pouco, otário")
+    total_tentativas = total_tentativas -1
